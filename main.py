7852f23251d09cf3e916a2a8d7de03728be4bcca
Musleh Ahmad <musleh.ahmad@baruchmail.cuny.edu>
2024-12-06 20:00:49 -0500

    Add main class for the project

---
+++ main.py
@@ -0,0 +1,8 @@
+from StockDetails import StockDetails
+
+symbol = input("Enter the stock symbol: ")
+
+stock = StockDetails(symbol)
+stock.display_summary()
+
+# Gary and nader add your classes here
