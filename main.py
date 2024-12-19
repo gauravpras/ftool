6e3c88b277886c0fcbb7783b40df8999097cf947
Gaurav Prasathong <54745995+gauravpras@users.noreply.github.com>
2024-12-18 19:44:59 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from StockDetails import StockDetails
+from dividends import Dividends
 
+symbol = input("Enter stock symbol(s). Use AAPL for one symbol or AAPL, MSFT, LUV for multiple symbols: ")
+
+stock = StockDetails(symbol)
+
+
+dividends = Dividends(symbol)
+dividends_df = dividends.dividends_data(symbol)
+dividends.visualize_dividends_data(dividends_df)
+
+# Gary and nader add your classes here
